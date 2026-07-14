e603753347753f417cbe71dd0190cf27b36936a5
MedzidJasarovic <medzidjasarevic1212@gmail.com>
2026-07-14 18:18:03 +0200

    tweaks on program.cs for the deployment

--- Program.cs
+++ Program.cs
@@ -84,6 +84,13 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+// Apply pending EF Core migrations on startup
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
